12bbebcdd3550b318881970225b42338591f46c8
Davis Peixoto <davis.peixoto@gmail.com>
2017-07-23 14:27:50 -0300

    Fixing namespaces for tests
    Signed-off-by: Davis Peixoto <davis.peixoto@gmail.com>

--- tests/TestCreateAuthorService.php
+++ tests/TestCreateAuthorService.php
@@ -6,6 +6,10 @@
  * Time: 2:12 PM
  */
 
+namespace DavisPeixoto\BlogCore\Tests;
+
+use PHPUnit_Framework_TestCase;
+
 class TestCreateAuthorService extends PHPUnit_Framework_TestCase
 {
     public function testService()
