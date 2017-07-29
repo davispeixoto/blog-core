0d89408d4fac5853c39c1087d762ce07910fe696
Davis Peixoto <davis.peixoto@gmail.com>
2017-07-29 13:58:41 -0300

    BC-6 Trail Services Tests 0h 11m
    OK. Done
    
    Signed-off-by: Davis Peixoto <davis.peixoto@gmail.com>

--- tests/Services/TestTrailServices.php
+++ tests/Services/TestTrailServices.php
@@ -8,14 +8,16 @@
 
 namespace DavisPeixoto\BlogCore\Tests\Services;
 
-use PHPUnit\Framework\TestCase;
 use DavisPeixoto\BlogCore\Entity\Author;
-use DavisPeixoto\BlogCore\Service\CreateAuthor;
-use DavisPeixoto\BlogCore\Service\EditAuthor;
-use DavisPeixoto\BlogCore\Service\DeleteAuthor;
-use DavisPeixoto\BlogCore\Service\GetAuthor;
-use DavisPeixoto\BlogCore\Service\ListAuthors;
-use DavisPeixoto\BlogCore\Repository\AbstractAuthorRepository;
+use DavisPeixoto\BlogCore\Entity\Post;
+use DavisPeixoto\BlogCore\Entity\Trail;
+use PHPUnit\Framework\TestCase;
+use DavisPeixoto\BlogCore\Service\CreateTrail;
+use DavisPeixoto\BlogCore\Service\EditTrail;
+use DavisPeixoto\BlogCore\Service\DeleteTrail;
+use DavisPeixoto\BlogCore\Service\GetTrail;
+use DavisPeixoto\BlogCore\Service\ListTrails;
+use DavisPeixoto\BlogCore\Repository\AbstractTrailRepository;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 use Psr\Log\LoggerInterface;
@@ -40,9 +42,29 @@ class TestTrailServices extends TestCase
     private $uuid;
 
     /**
-     * @Author
+     * @UuidInterface
+     */
+    private $authorUuid;
+
+    /**
+     * @UuidInterface
+     */
+    private $postUuid;
+
+    /**
+     * @Trail
      */
     private $trail;
+
+    /**
+     * @Author
+     */
+    private $author;
+
+    /**
+     * @Post
+     */
+    private $post;
 
     /**
      * @array
@@ -52,98 +74,102 @@ class TestTrailServices extends TestCase
     public function setUp()
     {
         $this->logger = $this->createMock(LoggerInterface::class);
-        $this->trailRepository = $this->getMockForAbstractClass(AbstractAuthorRepository::class);
+        $this->trailRepository = $this->getMockForAbstractClass(AbstractTrailRepository::class);
+        $this->authorUuid = Uuid::uuid4();
+        $this->author = new Author($this->authorUuid, 'Davis', 'email@example.org', 'Some string', new DateTime());
+        $this->postUuid = Uuid::uuid4();
         $this->uuid = Uuid::uuid4();
-        $this->trail = new Author($this->uuid, 'Davis', 'email@example.org', 'Some string', new DateTime());
+        $this->post = new Post($this->postUuid, 'A Post', 'Lorem ipsum', $this->author, [], null);
+        $this->trail = new Trail($this->uuid, 'A trail', 'An amazing trail', [$this->post]);
         $this->filters = [];
     }
 
-    public function testCreateAuthorService()
+    public function testCreateTrailService()
     {
         $this->trailRepository->expects($this->once())->method('save')->will($this->returnValue($this->uuid));
-        $service = new CreateAuthor($this->trailRepository, $this->trail, $this->logger);
+        $service = new CreateTrail($this->trailRepository, $this->trail, $this->logger);
         $this->assertEquals($this->uuid, $service->run(), 'called once!');
     }
 
-    public function testCreateAuthorServiceShouldReturnNullOnFailure()
+    public function testCreateTrailServiceShouldReturnNullOnFailure()
     {
         $this->logger->expects($this->once())->method('error');
         $this->trailRepository->expects($this->once())->method('save')->will($this->throwException(new Exception()));
-        $service = new CreateAuthor($this->trailRepository, $this->trail, $this->logger);
+        $service = new CreateTrail($this->trailRepository, $this->trail, $this->logger);
         $this->assertEquals(null, $service->run(), 'Test exception');
     }
 
-    public function testEditAuthorService()
+    public function testEditTrailService()
     {
         $this->trailRepository->expects($this->once())->method('save')->will($this->returnValue($this->uuid));
-        $service = new EditAuthor($this->trailRepository, $this->trail, $this->logger);
+        $service = new EditTrail($this->trailRepository, $this->trail, $this->logger);
         $this->assertEquals($this->uuid, $service->run(), 'called once!');
     }
 
-    public function testEditAuthorServiceShouldReturnNullOnFailure()
+    public function testEditTrailServiceShouldReturnNullOnFailure()
     {
         $this->logger->expects($this->once())->method('error');
         $this->trailRepository->expects($this->once())->method('save')->will($this->throwException(new Exception()));
-        $service = new EditAuthor($this->trailRepository, $this->trail, $this->logger);
+        $service = new EditTrail($this->trailRepository, $this->trail, $this->logger);
         $this->assertEquals(null, $service->run(), 'Test exception');
     }
 
-    public function testDeleteAuthorService()
+    public function testDeleteTrailService()
     {
         $this->trailRepository->expects($this->once())->method('delete')->will($this->returnValue(true));
-        $service = new DeleteAuthor($this->trailRepository, $this->trail, $this->logger);
+        $service = new DeleteTrail($this->trailRepository, $this->trail, $this->logger);
         $this->assertEquals(true, $service->run(), 'Success');
     }
 
-    public function testDeleteAuthorServiceReturningFalse()
+    public function testDeleteTrailServiceReturningFalse()
     {
         $this->trailRepository->expects($this->once())->method('delete')->will($this->returnValue(false));
-        $service = new DeleteAuthor($this->trailRepository, $this->trail, $this->logger);
+        $service = new DeleteTrail($this->trailRepository, $this->trail, $this->logger);
         $this->assertEquals(false, $service->run(), 'Success');
     }
 
-    public function testDeleteAuthorServiceShouldReturnFalseOnFailure()
+    public function testDeleteTrailServiceShouldReturnFalseOnFailure()
     {
         $this->logger->expects($this->once())->method('error');
         $this->trailRepository->expects($this->once())->method('delete')->will($this->throwException(new Exception()));
-        $service = new DeleteAuthor($this->trailRepository, $this->trail, $this->logger);
+        $service = new DeleteTrail($this->trailRepository, $this->trail, $this->logger);
         $this->assertEquals(false, $service->run(), 'Test exception');
     }
 
-    public function testGetAuthorService()
+    public function testGetTrailService()
     {
         $this->trailRepository->expects($this->once())->method('get')->will($this->returnValue($this->trail));
-        $service = new GetAuthor($this->trailRepository, $this->uuid, $this->logger);
+        $service = new GetTrail($this->trailRepository, $this->uuid, $this->logger);
         $this->assertEquals($this->trail, $service->run(), 'Success');
     }
 
-    public function testGetAuthorServiceReturningException()
+    public function testGetTrailServiceReturningException()
     {
         $this->logger->expects($this->once())->method('error');
         $this->trailRepository->expects($this->once())->method('get')->will($this->throwException(new Exception()));
-        $service = new GetAuthor($this->trailRepository, $this->uuid, $this->logger);
+        $service = new GetTrail($this->trailRepository, $this->uuid, $this->logger);
         $this->assertEquals(false, $service->run(), 'Exception');
     }
 
-    public function testGetAuthorServiceReturningFalse()
+    public function testGetTrailServiceReturningFalse()
     {
         $this->trailRepository->expects($this->once())->method('get')->will($this->returnValue(false));
-        $service = new GetAuthor($this->trailRepository, $this->uuid, $this->logger);
+        $service = new GetTrail($this->trailRepository, $this->uuid, $this->logger);
         $this->assertEquals(false, $service->run(), 'False');
     }
 
-    public function testGetAuthorsListService()
+    public function testGetTrailsListService()
     {
         $this->trailRepository->expects($this->once())->method('getList')->with($this->filters)->will($this->returnValue([$this->trail]));
-        $service = new ListAuthors($this->trailRepository, $this->filters, $this->logger);
+        $service = new ListTrails($this->trailRepository, $this->filters, $this->logger);
         $this->assertEquals([$this->trail], $service->run(), 'Success');
     }
 
-    public function testGetAuthorsListServiceException()
+    public function testGetTrailsListServiceException()
     {
         $this->logger->expects($this->once())->method('error');
         $this->trailRepository->expects($this->once())->method('getList')->with($this->filters)->will($this->throwException(new Exception()));
-        $service = new ListAuthors($this->trailRepository, $this->filters, $this->logger);
+        $service = new ListTrails($this->trailRepository, $this->filters, $this->logger);
         $this->assertEquals([], $service->run(), 'Exception');
     }
 }
